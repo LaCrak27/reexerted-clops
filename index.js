364b6b6e57a1f16ddfe965f96e73435bad951fd7
LaCrak 27 <ivancrak123@gmail.com>
2023-05-01 15:58:23 +0200

    Tricking servers

--- index.js
+++ index.js
@@ -1,59 +1,78 @@
 // Imported Modules
 require("dotenv").config();
-const { Client, IntentsBitField, EmbedBuilder, Collection } = require("discord.js");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const cron = require('node-cron');
-const fs = require('fs');
+const {
+  Client,
+  IntentsBitField,
+  EmbedBuilder,
+  Collection,
+} = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const cron = require("node-cron");
+const fs = require("fs");
+
+//Express.js server to get around limitations of hosting websites lmao
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
 
 // Discord client setup
 const serverIntents = new IntentsBitField(3276799);
-const bot = new Client({ intents: serverIntents })
-
+const bot = new Client({ intents: serverIntents });
 /**
  * Loads command objects from the commands folder
  * @author  (Airom) Airom42
  */
 bot.commands = new Collection();
-const commandFolders = fs.readdirSync('./commands');
+const commandFolders = fs.readdirSync("./commands");
 for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
-		command.category = folder;
-		if (command.data === undefined) {
-			bot.commands.set(command.name, command) // For non-slash commands
-		} else {
-			bot.commands.set(command.data.name, command) // For slash commands
-		}
-	}
+  const commandFiles = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    command.category = folder;
+    if (command.data === undefined) {
+      bot.commands.set(command.name, command); // For non-slash commands
+    } else {
+      bot.commands.set(command.data.name, command); // For slash commands
+    }
+  }
 }
 
 /**
  * Log a discord bot event in the Log Channel
  * @author  (Mgram) Marcus Ingram
  */
-async function botLog(embed,severity) {
-	let logColor
-	switch (severity) {
-		case 0:
-			logColor = '#42f569'
-			break;
-		case 1:
-			logColor = '#f5bf42'
-			break;
-		case 2:
-			logColor = '#f55142'
-			break;
-	}
-	embed.setColor(logColor)
-	.setTimestamp()
-	.setFooter({ text: 'Logs'});
-	try {
-		await bot.channels.cache.get(process.env.LOGCHANNEL).send({ embeds: [embed]})
-	} catch {
-		console.warn("ERROR: No Log Channel Environment Variable Found, Logging will not work.")
-	}
+async function botLog(embed, severity) {
+  let logColor;
+  switch (severity) {
+    case 0:
+      logColor = "#42f569";
+      break;
+    case 1:
+      logColor = "#f5bf42";
+      break;
+    case 2:
+      logColor = "#f55142";
+      break;
+  }
+  embed.setColor(logColor).setTimestamp().setFooter({ text: "Logs" });
+  try {
+    await bot.channels.cache
+      .get(process.env.LOGCHANNEL)
+      .send({ embeds: [embed] });
+  } catch {
+    console.warn(
+      "ERROR: No Log Channel Environment Variable Found, Logging will not work."
+    );
+  }
 }
 
 /**
@@ -61,122 +80,199 @@ async function botLog(embed,severity) {
  * @author  (Mgram) Marcus Ingram
  */
 async function deployCommands() {
-	const commands = [];
-	const commandFolders = fs.readdirSync('./commands');
-	for (const folder of commandFolders) {
-		const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-		for (const file of commandFiles) {
-			const command = require(`./commands/${folder}/${file}`);
-			command.category = folder;
-			if (command.data !== undefined) {
-				commands.push(command.data.toJSON());
-			}
-		}
-	}
-	const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
-	
-	try {
-		await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
-			{ body: commands },
-		);
+  const commands = [];
+  const commandFolders = fs.readdirSync("./commands");
+  for (const folder of commandFolders) {
+    const commandFiles = fs
+      .readdirSync(`./commands/${folder}`)
+      .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+      const command = require(`./commands/${folder}/${file}`);
+      command.category = folder;
+      if (command.data !== undefined) {
+        commands.push(command.data.toJSON());
+      }
+    }
+  }
+  const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
-		console.log('✅ Successfully registered application commands');
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENTID,
+        process.env.GUILDID
+      ),
+      { body: commands }
+    );
+
+    console.log("✅ Successfully registered application commands");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 /**
  * Event handler for Bot Login, manages post-login setup
  * @author  (Mgram) Marcus Ingram, (Airom42) Airom
  */
-bot.once("ready", async() => {
-	await deployCommands();
-	botLog(new EmbedBuilder().setDescription(`💡 R.Clops is now online! logged in as ${bot.user.tag}`).setTitle(`Reexerted Cyclops Online`),2);
-	console.log(`✅ R.Clops is now online! logged in as ${bot.user.tag}`)
-})
-
+bot.once("ready", async () => {
+  await deployCommands();
+  botLog(
+    new EmbedBuilder()
+      .setDescription(`💡 R.Clops is now online! logged in as ${bot.user.tag}`)
+      .setTitle(`Reexerted Cyclops Online`),
+    2
+  );
+  app.listen(port, () => console.log(`Listening on port ${port} for keeping the server alive and giving off status messages.`));
+  console.log(`✅ R.Clops is now online! logged in as ${bot.user.tag}`);
+});
 
 /**
  * Event handler for Slash Commands, takes interaction to test before executing command code.
  * @author  (Mgram) Marcus Ingram
  */
-bot.on('interactionCreate', async interaction => {
-	if (interaction.isCommand()) {
-		const command = bot.commands.get(interaction.commandName);
-		if (!command) {
-			console.log('WARNING: Unknown command detected.');
-			return;
-		}
-		let args;
-		if (interaction.options !== undefined) {
-			try {
-				args = JSON.stringify(interaction.options.data)
-			} catch (err) {
-				console.log(`WARNING: Unable to create arguments for legacy command '${interaction.commandName}', this may not affect modern slash commands: ${err}`)
-			}
-		}
-		try {
-			botLog(new EmbedBuilder().setDescription(`Command used by ${interaction.user.tag} - Command ` + "`" + `${interaction.commandName}` + "`" + ` with arguments: ` + "`" + `${args}` + "`"),0);
-			await command.execute(interaction, args);
-		} catch (error) {
-			console.error(error);
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+bot.on("interactionCreate", async (interaction) => {
+  if (interaction.isCommand()) {
+    const command = bot.commands.get(interaction.commandName);
+    if (!command) {
+      console.log("WARNING: Unknown command detected.");
+      return;
+    }
+    let args;
+    if (interaction.options !== undefined) {
+      try {
+        args = JSON.stringify(interaction.options.data);
+      } catch (err) {
+        console.log(
+          `WARNING: Unable to create arguments for legacy command '${interaction.commandName}', this may not affect modern slash commands: ${err}`
+        );
+      }
+    }
+    try {
+      botLog(
+        new EmbedBuilder().setDescription(
+          `Command used by ${interaction.user.tag} - Command ` +
+            "`" +
+            `${interaction.commandName}` +
+            "`" +
+            ` with arguments: ` +
+            "`" +
+            `${args}` +
+            "`"
+        ),
+        0
+      );
+      await command.execute(interaction, args);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 
-	if (interaction.isButton()) {
-		botLog(new EmbedBuilder().setDescription(`Button triggered by user **${interaction.user.tag}** - Button ID: ${interaction.customId}`),0);
-		if (interaction.customId.startsWith("submission")) {
-			interaction.deferUpdate();
-			leaderboardInteraction(interaction);
-			return;
-		}
-		if (interaction.customId.startsWith("fcc")) {
-			interaction.deferUpdate();
-			fccinteraction(interaction);
-			return;
-		}
-	}
+  if (interaction.isButton()) {
+    botLog(
+      new EmbedBuilder().setDescription(
+        `Button triggered by user **${interaction.user.tag}** - Button ID: ${interaction.customId}`
+      ),
+      0
+    );
+    if (interaction.customId.startsWith("submission")) {
+      interaction.deferUpdate();
+      leaderboardInteraction(interaction);
+      return;
+    }
+    if (interaction.customId.startsWith("fcc")) {
+      interaction.deferUpdate();
+      fccinteraction(interaction);
+      return;
+    }
+  }
 });
 
 // Audit Logging Events
 
 // Message Deleted by user
-bot.on('messageDelete', async message => {
-	try {
-		botLog(new EmbedBuilder().setDescription(`Message deleted by user: ${message.author}` + '```' + `${message.content}` + '```').setTitle(`Message Deleted 🗑️`),1)
-	} catch (err) {
-		botLog(new EmbedBuilder().setDescription(`Something went wrong while logging a Deletion event: ${err}`).setTitle(`Logging Error`),2);
-	}
-})
+bot.on("messageDelete", async (message) => {
+  try {
+    botLog(
+      new EmbedBuilder()
+        .setDescription(
+          `Message deleted by user: ${message.author}` +
+            "```" +
+            `${message.content}` +
+            "```"
+        )
+        .setTitle(`Message Deleted 🗑️`),
+      1
+    );
+  } catch (err) {
+    botLog(
+      new EmbedBuilder()
+        .setDescription(
+          `Something went wrong while logging a Deletion event: ${err}`
+        )
+        .setTitle(`Logging Error`),
+      2
+    );
+  }
+});
 
 // Message Updated by user
-bot.on('messageUpdate', (oldMessage, newMessage) => {
-	if (oldMessage != newMessage && oldMessage.author.id != process.env.CLIENTID) {
-		botLog(new EmbedBuilder().setDescription(`Message updated by user: ${oldMessage.author}` + '```' + `${oldMessage}` + '```' + `Updated Message:` + '```' + `${newMessage}` + '```' + `Message Link: ${oldMessage.url}`).setTitle(`Message Updated 📝`),1)
-	}
+bot.on("messageUpdate", (oldMessage, newMessage) => {
+  if (
+    oldMessage != newMessage &&
+    oldMessage.author.id != process.env.CLIENTID
+  ) {
+    botLog(
+      new EmbedBuilder()
+        .setDescription(
+          `Message updated by user: ${oldMessage.author}` +
+            "```" +
+            `${oldMessage}` +
+            "```" +
+            `Updated Message:` +
+            "```" +
+            `${newMessage}` +
+            "```" +
+            `Message Link: ${oldMessage.url}`
+        )
+        .setTitle(`Message Updated 📝`),
+      1
+    );
+  }
 });
 
 // User leaving server
-bot.on('guildMemberRemove', member => {
-	let roles = ``
-	member.roles.cache.each(role => roles += `${role}\n`)
-	botLog(new EmbedBuilder()
-	.setDescription(`User ${member.user.tag}(${member.displayName}) has left or was kicked from the server.`)
-	.setTitle(`User Left/Kicked from Server`)
-	.addFields(
-		{ name: `ID`, value: `${member.id}`},
-		{ name: `Date Joined`, value: `<t:${(member.joinedTimestamp/1000) >> 0}:F>`},
-		{ name: `Roles`, value: `${roles}`},
-	),2)
-})
-bot.login(process.env.TOKEN)
+bot.on("guildMemberRemove", (member) => {
+  let roles = ``;
+  member.roles.cache.each((role) => (roles += `${role}\n`));
+  botLog(
+    new EmbedBuilder()
+      .setDescription(
+        `User ${member.user.tag}(${member.displayName}) has left or was kicked from the server.`
+      )
+      .setTitle(`User Left/Kicked from Server`)
+      .addFields(
+        { name: `ID`, value: `${member.id}` },
+        {
+          name: `Date Joined`,
+          value: `<t:${(member.joinedTimestamp / 1000) >> 0}:F>`,
+        },
+        { name: `Roles`, value: `${roles}` }
+      ),
+    2
+  );
+});
+bot.login(process.env.TOKEN);
 
 // General error handling
-process.on('uncaughtException', function (err) {
-	console.log(`⛔ Fatal error occured:`)
-	console.error(err);
-	bot.channels.cache.get(process.env.LOGCHANNEL).send({ content: `⛔ Fatal error experienced: ${err}` })
+process.on("uncaughtException", function (err) {
+  console.log(`⛔ Fatal error occured:`);
+  console.error(err);
+  bot.channels.cache
+    .get(process.env.LOGCHANNEL)
+    .send({ content: `⛔ Fatal error experienced: ${err}` });
 });
