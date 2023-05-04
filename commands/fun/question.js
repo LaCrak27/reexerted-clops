c35584964027d83b3e395532d195c9a774b7bd5a
LaCrak 27 <ivancrak123@gmail.com>
2023-05-04 15:22:01 +0200

    ai is now more on-topic

--- commands/fun/question.js
+++ commands/fun/question.js
@@ -17,15 +17,14 @@ module.exports = {
     async execute(interaction) {
         const temp = interaction.options.data.find(arg => arg.name === 'temperature').value / 100;
         var arr1 = new Array();
-        arr1.push("Human:");
-        arr1.push("Reexerted Cyclops:");
+        arr1.push("   ");
         // AI bot setup
         const cohere = require('cohere-ai');
         cohere.init(process.env.APIKEY)
         const generateResponse = await cohere.generate({
             model: "base-light",
             prompt: `This is a discussion between a human and Reexerted Cyclops. 
-            Reexerted Cyclops is very nice and empathetic, but he only responds one time, and no further questions are asked after that.
+            Reexerted Cyclops is very nice and empathetic, and he responds precisely without going off-topic.
             
             Human: ${interaction.options.data.find(arg => arg.name === 'question').value}
             Reexerted Cyclops:`,
@@ -35,16 +34,8 @@ module.exports = {
             frequency_penalty: 1.0,
             stop_sequences: arr1,
         });
-        const generations = generateResponse.body.generations[0].text;
-        if (generations.endsWith("Human:")) {
-            var text = generations.slice(0, -6);
-        }
-        else {
-            if (generations.endsWith("Reexerted Cyclops:")) {
-                var text = generations.slice(0, -18);
-            }
-            var text = generations;
-        }
+        const generations = generateResponse.body.generations[0].text; 
+        var text = generations;
         //console.log(generations);
         try {
             await interaction.reply({ content: text })
